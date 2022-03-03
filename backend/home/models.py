11aa9eb4d1f9e708e14e4864cb282f4e86fa6aaa
Crowdbotics <engineering@crowdbotics.com>
2022-03-03 08:02:11 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+
+
+class Home(models.Model):
+    "Generated Model"
+    user = models.ManyToManyField(
+        "users.User",
+        related_name="home_user",
+    )
